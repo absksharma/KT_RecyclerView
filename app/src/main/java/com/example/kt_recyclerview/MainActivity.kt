320a415b9d4f148571e24aad85c2a44cab61e3b8
Abhishek <absk.sharma7@gmail.com>
2021-10-18 23:00:12 +0530

    ___

--- app/src/main/java/com/example/kt_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/kt_recyclerview/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.kt_recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kt_recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
